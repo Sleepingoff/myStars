63cf57de1ee957a779a5169cb574997f713da06a
Sleepingoff <justjh30@gmail.com>
2024-07-11 20:03:50 +0900

    add global styles

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+        vertical-align: middle;
+    }
+`;
+
+export default GlobalStyles;
